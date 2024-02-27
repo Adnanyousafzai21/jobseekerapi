24fc74c9ad72199781ece5d7168d9ecdcb9d4fee
Adnanyousafzai21 <www.adnanrafiq@gmail.com>
2024-02-27 20:36:34 +0500

    SFDFAADFA

--- src/app.js
+++ src/app.js
@@ -26,9 +26,7 @@ app.get("/", (req, res) => {
     res.send({ message: "Congratulations, your app is working perfectly!" });
 });
 
-// app.get("/", (req, res)=>{
-//     res.send("congratulation your app is working perfectly")
-// })
+
 
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/application", applicationRouter)
