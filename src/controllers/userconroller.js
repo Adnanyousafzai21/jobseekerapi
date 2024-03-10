7a7f482732d586bcc42162d3585b0ea157f1693e
Adnanyousafzai21 <www.adnanrafiq@gmail.com>
2024-03-11 00:00:59 +0500

    ADFAFA

--- src/controllers/userconroller.js
+++ src/controllers/userconroller.js
@@ -1,16 +1,16 @@
 import User from "../models/usermodel.js"
-import {sendToken} from "../utils/jwtToken.js"
+import { sendToken } from "../utils/jwtToken.js"
 const Register = async (req, res, next) => {
-    try{
+    try {
         const { name, email, password, role, phone } = req.body
         if (
             !name || !email || !role || !phone || !password
         ) {
-         return   res.status(400).send({message:"all field are required"})
+            return res.status(400).send({ message: "all field are required" })
         }
-        const userExist = await User.findOne({email})
-        if(userExist) return res.status(400).send({message:"the user is already exist"})
-    
+        const userExist = await User.findOne({ email })
+        if (userExist) return res.status(400).send({ message: "the user is already exist" })
+
         const createdUser = await User.create({
             name,
             password,
@@ -18,53 +18,54 @@ const Register = async (req, res, next) => {
             phone,
             role
         })
-    
+
         const user = await User.findById(createdUser._id).select("-password")
-        if(!user) return res.status(500).send({message:"somthing went wrong while creating user"})
-    
-        sendToken(user, res, "user registered successfuly" , 200)
-    }catch(error){
-console.log(error)
-res.status(400).send({message:"error is accoure while registering"})
-   
+        if (!user) return res.status(500).send({ message: "somthing went wrong while creating user" })
+
+        sendToken(user, res, "user registered successfuly", 200)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({ message: "error is accoure while registering" })
+
     }
- const Login = async (req, res, next)=>{
-
-   try{
-    const {email, password ,role}= req.body 
-    if(!email || !password) return res.status(400).send({message:"please entered email and password"})
-
-    const user = await User.findOne({email})
-    if(!user) return res.status(404).send({message:"email or password is not correct"})
-
-
-    const isPasswordMatched = await user.isPasswordCorrect(password)
-    if(!isPasswordMatched) return res.status(400).send({message:"email or password does'nt match"})
-
-    if(user.role!==role) return res.status(404).send({message:`user with provided email or ${role} is not found`})
-
-
-    sendToken(user, res, "logedIn successfuly", 200)
-    
-   }catch(error){
-console.log(error)
-   }
-    
 }
+    const Login = async (req, res, next) => {
 
-const logedOut = async (req, res, next)=>{
-    res.status(201).cookie("token","",{
-        httpOnly: true,
-        expiresIn: new Date(Date.now()),
-    }).send({success: true, message:"LogedOut successfuly"})
-}
+        try {
+            const { email, password, role } = req.body
+            if (!email || !password) return res.status(400).send({ message: "please entered email and password" })
 
-const getuser =async (req, res, next)=>{
-    const user= req.user
-    
-    if(!user) return res.status(500).send({message:"user not found"})
-     res.status(200).send({success: true , user})
-}
+            const user = await User.findOne({ email })
+            if (!user) return res.status(404).send({ message: "email or password is not correct" })
 
 
-export {Register, Login, logedOut,getuser}
+            const isPasswordMatched = await user.isPasswordCorrect(password)
+            if (!isPasswordMatched) return res.status(400).send({ message: "email or password does'nt match" })
+
+            if (user.role !== role) return res.status(404).send({ message: `user with provided email or ${role} is not found` })
+
+
+            sendToken(user, res, "logedIn successfuly", 200)
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    const logedOut = async (req, res, next) => {
+        res.status(201).cookie("token", "", {
+            httpOnly: true,
+            expiresIn: new Date(Date.now()),
+        }).send({ success: true, message: "LogedOut successfuly" })
+    }
+
+    const getuser = async (req, res, next) => {
+        const user = req.user
+
+        if (!user) return res.status(500).send({ message: "user not found" })
+        res.status(200).send({ success: true, user })
+    }
+
+
+    export { Register, Login, logedOut, getuser }
