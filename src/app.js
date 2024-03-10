78fc87dcffea969569c3c781c3fec5cc9a7e1f7a
Adnanyousafzai21 <www.adnanrafiq@gmail.com>
2024-03-10 23:49:18 +0500

    last i wish

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import cors from "cors"
 const app = express()
 
 const corsOptions = {
-    origin: "*", 
+    origin: "https://jobseekerfrontend.vercel.app", 
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   };
